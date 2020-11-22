12349c82cbf69e229dc2ba29262b66d41fd41b7d
masa <masatoshi.sboard@gmail.com>
2020-11-23 00:42:56 +0900

    Done a

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -4,5 +4,20 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        n: usize,
+        p: [usize; n],
     }
+    let mut dp = vec![false; n * 100 + 1];
+    dp[0] = true;
+    for i in p {
+        for j in (0..dp.len()).rev() {
+            if i + j >= dp.len() {
+                continue;
+            }
+            if dp[j] {
+                dp[i + j] = true;
+            }
+        }
+    }
+    println!("{}", dp.into_iter().filter(|&x| x).count());
 }
